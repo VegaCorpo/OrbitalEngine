90e2bfc7a68c3f07de325675dd834b003096dd38
Lucas <lucas.ludovic@epitech.eu>
2026-02-24 10:09:15 +0100

    chore: add basic cmakelist for core and physics
    package-lock.cmake added and eigen retrieved

--- package-lock.cmake
+++ package-lock.cmake
@@ -19,3 +19,10 @@
 #         SYSTEM YES
 #         EXCLUDE_FROM_ALL YES
 # )
+# Eigen
+CPMDeclarePackage(Eigen
+        GIT_TAG 5.0.1
+        GITLAB_REPOSITORY libeigen/eigen
+        SYSTEM YES
+        EXCLUDE_FROM_ALL YES
+)
