692de0c667d0e5422909b6359a4bc72c237604e6
Lucas <lucas.ludovic@epitech.eu>
2026-02-24 06:20:09 +0100

    chore: add main cmakelists

---
+++ package-lock.cmake
@@ -0,0 +1,21 @@
+# # boost
+# CPMDeclarePackage(boost
+#         URL https://github.com/boostorg/boost/releases/download/boost-1.89.0/boost-1.89.0-cmake.tar.gz
+#         SYSTEM YES
+#         EXCLUDE_FROM_ALL YES
+# )
+#
+# # raylib
+# CPMDeclarePackage(raylib
+#         GIT_TAG 5.5
+#         GITHUB_REPOSITORY raysan5/raylib
+#         SYSTEM YES
+#         EXCLUDE_FROM_ALL YES
+# )
+# # raylib-cpp
+# CPMDeclarePackage(raylib-cpp
+#         VERSION 5.5.0
+#         GITHUB_REPOSITORY RobLoach/raylib-cpp
+#         SYSTEM YES
+#         EXCLUDE_FROM_ALL YES
+# )
