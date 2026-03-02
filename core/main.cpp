7371037204c1cd6aa4031eaf2f168561655fb11a
Yuuko <146713844+TilioHuart@users.noreply.github.com>
2026-03-02 16:53:14 +0100

    chore(submodule): initialize project submodules (#122)
    * ci: add workflow to auto-update submodules on repository dispatch
    
    * feat(submodule): add core module
    
    * feat(submodule): add physics module
    
    * feat(submodule): add ui module
    
    * feat(submodule): add loader module
    
    * feat(submodule): add inputs module
    
    * feat(submodule): add renderer module
    
    * chore(submodule): update ui module
    
    * chore(build): integrate submodules into build pipeline

--- core/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-    std::cout << "Hello from Engine" << std::endl;
-    return 0;
-}
--- render/RenderEngine.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace render {
-    class RenderEngine {};
-} // namespace render
