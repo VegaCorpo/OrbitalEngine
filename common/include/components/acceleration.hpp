bddb5dfe091ec992d0659547ca4ea8a16b51a29e
Yuuko <tilio.huart@epitech.eu>
2026-03-03 14:46:48 +0100

    feat: add basic components

---
+++ common/include/components/acceleration.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace components {
+    struct Acceleration
+    {
+            float x = 0;
+            float y = 0;
+            float z = 0;
+    };
+} // namespace components
